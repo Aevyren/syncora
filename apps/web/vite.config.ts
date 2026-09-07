af8aa331c083e63489c129ecf45b94fa6843bdc9
flameboy <154361051+flameboy-dev@users.noreply.github.com>
2026-09-07 19:22:51 +0530

    project scaffolded

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,22 @@
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+});
